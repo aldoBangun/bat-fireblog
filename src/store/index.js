4119908636ad2ad88d2e2131347e56862a14846d
aldoBangun <punk.aldo68@gmail.com>
2021-07-18 16:52:57 +0800

    add navigation menu states

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,45 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 const store = createStore({
    state() {
       return {
-         
-      }
+         isActiveNav: false,
+         navMenu: [
+            {
+               label: "Home",
+               link: "#",
+            },
+            {
+               label: "Blogs",
+               link: "#",
+            },
+            {
+               label: "Create Post",
+               link: "#",
+            },
+            {
+               label: "Sign Up",
+               link: "#",
+               button: true,
+            },
+         ],
+      };
    },
    getters: {
-      
+      isActiveNav(state) {
+         return state.isActiveNav
+      },
+      navMenu(state) {
+         return state.navMenu
+      },
    },
    mutations: {
-      
+      SET_ACTIVE_NAV(state, payload) {
+         state.isActiveNav = payload;
+      },
    },
-   actions: {
-      
-   },
+   actions: {},
    modules: {},
-})
+});
 
-export default store
+export default store;
